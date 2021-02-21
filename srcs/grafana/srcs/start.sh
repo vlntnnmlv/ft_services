1d42d9b89845ce7e5e196ba9095ace0eaf20e7e8
Отец <father@father.com>
2021-02-21 17:29:40 +0300

    Hopefully fixed grafana

--- srcs/grafana/srcs/start.sh
+++ srcs/grafana/srcs/start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/usr/share/grafana/bin/grafana-server --homepath=/usr/share/grafana
+# /usr/share/grafana/bin/grafana-server --homepath=/usr/share/grafana
+# /usr/share/grafana/grafana-5.0.4/bin/grafana-server
+/usr/share/grafana/bin/grafana-server -homepath "/usr/share/grafana/" -config "/usr/share/grafana/conf/custom.ini"
